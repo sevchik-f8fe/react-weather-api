2a2bbd54c3deed0224e43b91e3dbb3187a9c71b6
sevchik-f8fe <kononovseva0627@gmail.com>
2024-05-30 23:38:42 +0300

    zaeb

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Root from './routes/root.jsx';
 import './index.css'
 import Header from './components/Header.jsx';
 import Footer from './components/Footer.jsx';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Root />,
+//   },
+// ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Header />
-    <RouterProvider router={router} />
+    <Root />
+    {/* <RouterProvider router={router} /> */}
     <Footer />
   </React.StrictMode>,
 )
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,28 +1,27 @@
 import { useState, useEffect } from "react";
 import { Line, LineChart, CartesianGrid, XAxis, YAxis, Tooltip } from "recharts";
 
-import { APP_ID, APP_KEY } from "../data/data";
+import { API_KEY, ICONS, DAYS_WEEK } from "../data/data";
 
 const Root = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [data, setData] = useState({});
-    const [location, setLocation] = useState({
-        longitude: -0.12,
-        latitude: 51.50,
-    });
-    // const url = `http://api.weatherunlocked.com/api/current/51.50,-0.12?app_id=65c6ca61&app_key=b465832dcef33b48be3c9380d4435a80`;
-    const url = `http://api.weatherunlocked.com/api/forecast/${location.latitude},${location.longitude}?lang=ru&app_id=${APP_ID}&app_key=${APP_KEY}`;
+    // const [isLoading, setIsLoading] = useState(false);
+    const [data, setData] = useState([]);
+    // const [location, setLocation] = useState({
+    //     longitude: 30.18,
+    //     latitude: 59.56,
+    // });
+
+    const [isCelsius, setIsCelisius] = useState(true);
+
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/38.9697,-77.385?key=${API_KEY}&lang=ru`;
 
     useEffect(() => {
-        setIsLoading(true);
-
         fetch(url)
             .then(response => response.json())
-            .then(text => setData(text.Days))
-            // .then(d => console.log(d.Days))
-            .catch(err => console.log(err))
-            .finally(() => setIsLoading(false));
-    }, [url]);
+            .then(
+                (json) => console.log(json),
+                (err) => console.log(err))
+    }, []);
 
     const data1 = [
         { name: 'Page A', uv: 400 },
@@ -35,8 +34,55 @@ const Root = () => {
     ];
 
     return (
-        <>
-            <h1 className="text-purple-800 "> {isLoading ? "fff" : data[0].Timeframes[0].date}</h1>
+        <main className="w-4/5 mx-auto my-12">
+            <div className="flex items-center justify-between">
+
+                <div className="flex gap-4 items-center">
+                    {/* <img src={ICONS.get(currentFrame.wx_code)} className="w-20 h-20" alt="weather icon" /> */}
+                    <div className="flex items-start">
+                        <span className="text-8xl text-zinc-800">
+                            {
+                                isCelsius ? (
+                                    12
+                                ) : (
+                                    34
+                                )
+                            }
+                        </span>
+
+                        <div className="flex gap-1 mt-3">
+                            <span
+                                className={`text-2xl font-medium cursor-pointer ${isCelsius ? "text-zinc-700" : "text-zinc-400"}`}
+                                onClick={() => setIsCelisius(true)}
+                            >
+                                &#176;C
+                            </span>
+                            <span className="text-2xl font-medium text-zinc-700">
+                                |
+                            </span>
+                            <span
+                                className={`text-2xl font-medium cursor-pointer ${!isCelsius ? "text-zinc-700" : "text-zinc-400"}`}
+                                onClick={() => setIsCelisius(false)}
+                            >
+                                &#176;F
+                            </span>
+                        </div>
+
+                        <div className="flex flex-col ml-2 mt-3">
+                            <span className="text-sm text-zinc-400">Вероятность осадков: 0 %</span>
+                            <span className="text-sm text-zinc-400">Влажность: 0 %</span>
+                            <span className="text-sm text-zinc-400">Ветер: 0 м/с</span>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="flex self-start mt-3 items-end flex-col">
+                    <span>Погода</span>
+                    <CurrentDate />
+                    <span>описание</span>
+                </div>
+            </div>
             <LineChart className="m-8" margin={{ top: 5, right: 20, bottom: 5, left: 0 }} width={600} height={300} data={data1}>
                 <Line type="monotone" dataKey="uv" stroke="#8884d8" />
                 <CartesianGrid stroke="#ccc" strokeDasharray="2 5" />
@@ -44,8 +90,14 @@ const Root = () => {
                 <YAxis />
                 <Tooltip />
             </LineChart>
-        </>
+        </main>
     )
+}
+
+const CurrentDate = () => {
+    let now = new Date();
+
+    return <span>{DAYS_WEEK[now.getDay()]} {now.getHours()}:{now.getMinutes()}</span>;
 }
 
 export default Root;
